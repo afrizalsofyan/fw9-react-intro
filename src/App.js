83c37cfc0c9b17e64e02d70ed731f7990231dbca
afrizalsofyan <afrizalafaandy@gmail.com>
2022-07-21 10:32:28 +0700

    fix: update task 2 fetch axios

--- src/App.js
+++ src/App.js
@@ -1,94 +1,35 @@
-import React from 'react';
-import {Col, Row, Button} from 'react-bootstrap';
+import React from "react";
+import { Row, Col } from "react-bootstrap";
 
-const ResultNum = ({value})=>{  
-  return(
-    <>
-      {value}
-    </>
-  )
-}
+const axios = require("axios");
 
-const ErrNegativeNum = ({errMsg})=>{
-  return  (
-    <>
-      {errMsg}  
-    </>
-  )
-}
-
-const App = () =>{
-  const [value, setValue] = React.useState(0);
+const App = () => {
+  const [data, setData] = React.useState([]);
+  const getData = async () => {
+    const res = await axios.get("https://rickandmortyapi.com/api/character");
+    setData(res.data.results);
+  };
+  React.useEffect(() => {
+    getData();
+  }, []);
   return (
-    <Row className='min-vh-100 gy-0 gx-0'>
-      <Col className='d-flex flex-column justify-content-center align-items-center'>
-        <Row className='w-50 gx-0'>
-          <Col className='d-flex justify-content-center'>
-            <Button onClick={()=>value < 1 ? value : setValue(value-1)}>-</Button>
-          </Col>
-          <Col className='d-flex flex-column justify-content-center text-center'>
-            <ResultNum value={value}></ResultNum>
-            {/* {value < 0 ? setValue(value) : value} */}
-          </Col>
-          <Col className='d-flex justify-content-center'>
-            <Button onClick={()=>setValue(value+1)}>+</Button>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            {value < 1 ? <ErrNegativeNum errMsg={'Min number 0'}></ErrNegativeNum> : null}
-          </Col>
-        </Row>
-      </Col>
-    </Row>
-  )
-}
-
+    <>
+      <Row className="gx-0 gy-0">
+        {data.forEach((element) => {
+          // console.log(element.name);
+          return (
+            <Col md={4} className="p-5">
+              <figure>
+                <img src={element.image} alt={`Image_${element.name}`} className="w-100" />
+                <figcaption className="fs-3 text-center pt-3">
+                  {element.name}
+                </figcaption>
+              </figure>
+            </Col>
+          );
+        })}
+      </Row>
+    </>
+  );
+};
 export default App;
-
-// import { Component } from "react";
-// const SeriesList = ({series, children}) => {
-//   return (
-//     // react fragment
-//     <>
-//       {children} 
-//       <li>{series.name}</li>
-//     </>
-//   )
-// }
-
-// const App = () => {
-//   return (
-//     <ol>
-//       <SeriesList series={{name: 'aaaa'}}>aku</SeriesList>
-//     </ol>
-//   )
-// }
-
-// export default App;
-
-// class Series extends Component{
-//   render(){
-//     return (
-//       <>
-//         {this.props.children}
-//         <li>{this.props.eps.ep}</li>
-//       </>
-//     )
-//   }
-// }
-
-// class App extends Component {
-//   state ={
-//     isShow: true
-//   }
-//   render(){
-//     return (
-//       <ol>
-//         {this.state.isShow && <Series eps={{ep: 4}}>abc</Series>}
-//         <button onClick={()=>this.setState({isShow: !this.state.isShow})}>click</button>
-//       </ol>
-//     )
-//   }
-// }
-// export default App;
---
+++ src/IncreaseDecrease.jsx
@@ -0,0 +1,94 @@
+import React from 'react';
+import {Col, Row, Button} from 'react-bootstrap';
+
+const ResultNum = ({value})=>{  
+  return(
+    <>
+      {value}
+    </>
+  )
+}
+
+const ErrNegativeNum = ({errMsg})=>{
+  return  (
+    <>
+      {errMsg}  
+    </>
+  )
+}
+
+const IncreaseDecrease = () =>{
+  const [value, setValue] = React.useState(0);
+  return (
+    <Row className='min-vh-100 gy-0 gx-0'>
+      <Col className='d-flex flex-column justify-content-center align-items-center'>
+        <Row className='w-50 gx-0'>
+          <Col className='d-flex justify-content-center'>
+            <Button onClick={()=>value < 1 ? value : setValue(value-1)}>-</Button>
+          </Col>
+          <Col className='d-flex flex-column justify-content-center text-center'>
+            <ResultNum value={value}></ResultNum>
+            {/* {value < 0 ? setValue(value) : value} */}
+          </Col>
+          <Col className='d-flex justify-content-center'>
+            <Button onClick={()=>setValue(value+1)}>+</Button>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            {value < 1 ? <ErrNegativeNum errMsg={'Min number 0'}></ErrNegativeNum> : null}
+          </Col>
+        </Row>
+      </Col>
+    </Row>
+  )
+}
+
+export default IncreaseDecrease;
+
+// import { Component } from "react";
+// const SeriesList = ({series, children}) => {
+//   return (
+//     // react fragment
+//     <>
+//       {children} 
+//       <li>{series.name}</li>
+//     </>
+//   )
+// }
+
+// const IncreaseDecrease = () => {
+//   return (
+//     <ol>
+//       <SeriesList series={{name: 'aaaa'}}>aku</SeriesList>
+//     </ol>
+//   )
+// }
+
+// export default IncreaseDecrease;
+
+// class Series extends Component{
+//   render(){
+//     return (
+//       <>
+//         {this.props.children}
+//         <li>{this.props.eps.ep}</li>
+//       </>
+//     )
+//   }
+// }
+
+// class IncreaseDecrease extends Component {
+//   state ={
+//     isShow: true
+//   }
+//   render(){
+//     return (
+//       <ol>
+//         {this.state.isShow && <Series eps={{ep: 4}}>abc</Series>}
+//         <button onClick={()=>this.setState({isShow: !this.state.isShow})}>click</button>
+//       </ol>
+//     )
+//   }
+// }
+// export default IncreaseDecrease;
